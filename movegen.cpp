a957d94b7b31204ed4ad3bbbaf441bd27e46a600
Roman Jackson <romanj@uchicago.edu>
2025-11-30 01:33:12 -0600

    added move generation

---
+++ movegen.cpp
@@ -0,0 +1,29 @@
+#include "movegen.h"
+#include "rank.h"
+#include "cuarenta.h"
+#include <vector>
+
+namespace Cuarenta {
+
+std::vector<Move> generate_all_moves(const Table& table, const Hand& hand) {   
+
+    std::vector<Move> moves;
+    moves.reserve(MAX_MOVES_PER_TABLE);
+
+    const RankMask low_table_mask = table.cards & LOW_MASK;
+
+    for (const Rank& card : hand.cards) {
+
+        moves.push_back(Move{to_mask(card)});
+        const int rank_idx { rank_to_int(card) };
+
+        for (const RankMask addition_pattern : ADDITIONS_BY_RANK[rank_idx]) {
+            if ((addition_pattern & low_table_mask) == addition_pattern) {
+                moves.push_back(Move{addition_pattern | to_mask(card)});
+            }
+        }
+    }
+    return moves;
+}
+
+}
---
+++ movegen.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "cuarenta.h"
+#include <vector>
+
+namespace Cuarenta {
+
+
+constexpr std::initializer_list<RankMask> ADDITIONS_BY_RANK[] = {
+    {},
+    {},
+    {},
+    {   to_mask(Rank::Ace) | to_mask(Rank::Two)   },
+    {   to_mask(Rank::Ace) | to_mask(Rank::Three) }, 
+    {
+        to_mask(Rank::Ace) | to_mask(Rank::Four),
+        to_mask(Rank::Two) | to_mask(Rank::Three) }, 
+    {
+        to_mask(Rank::Ace)  | to_mask(Rank::Five),
+        to_mask(Rank::Four) | to_mask(Rank::Two),
+        to_mask(Rank::Ace)  | to_mask(Rank::Two) | to_mask(Rank::Three) }, 
+    {
+        to_mask(Rank::Ace)   | to_mask(Rank::Six),
+        to_mask(Rank::Two)   | to_mask(Rank::Five),
+        to_mask(Rank::Three) | to_mask(Rank::Four),
+        to_mask(Rank::Ace)   | to_mask(Rank::Two) | to_mask(Rank::Four) },
+    {},
+    {},
+    {}
+};
+
+constexpr RankMask LOW_MASK { to_mask(Rank::Ace)   | to_mask(Rank::Two)  |
+                              to_mask(Rank::Three) | to_mask(Rank::Four) |
+                              to_mask(Rank::Five)  | to_mask(Rank::Six) };
+
+std::vector<Move> generate_all_moves(const Table& table, const Hand& hand);
+}
