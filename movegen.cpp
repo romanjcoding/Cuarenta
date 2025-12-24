91668e74735bf68dc5b4a6a9f0a6aba5c1997c77
Roman Jackson <romanj@uchicago.edu>
2025-12-24 02:49:31 -0800

    Added helpers to avoid spaghetti code :)

--- movegen.cpp
+++ movegen.cpp
@@ -13,16 +13,15 @@ MoveList generate_all_moves(const Game_State& game) {
 
     for (const Rank& card : current_player_state(game).hand.cards) {
 
-        if (available_moves.n >= MAX_MOVES_PER_TABLE) break; // not needed?
-        available_moves.moves[available_moves.n++] = Move{ to_mask(card) };
+        if (available_moves.size >= MAX_MOVES_PER_TABLE) break; // not needed?
+        available_moves.push_back(Move{to_mask(card)});
 
         const int rank_idx { rank_to_int(card) };
 
         for (const RankMask addition_pattern : ADDITIONS_BY_RANK[rank_idx]) {
             if ((addition_pattern & low_table_mask) == addition_pattern) {
-                if (available_moves.n >= MAX_MOVES_PER_TABLE) break; // not needed?
-                available_moves.moves[available_moves.n++] = 
-                    Move{ addition_pattern | to_mask(card) };
+                if (available_moves.size >= MAX_MOVES_PER_TABLE) break; // not needed?
+                available_moves.push_back(Move{addition_pattern | to_mask(card)});
             }
         }
     }
