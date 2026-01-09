d0da3ab4403fe5b87881da29dae25dab4759ca4d
Roman Jackson <romanj@uchicago.edu>
2026-01-09 14:28:38 -0600

    Movegen uses utils::dynamic_array

--- movegen.cpp
+++ movegen.cpp
@@ -1,4 +1,5 @@
 #include "movegen.h"
+#include "dynamic_array.h"
 #include "rank.h"
 #include "cuarenta.h"
 
@@ -7,25 +8,25 @@
 
 namespace Cuarenta {
 
-MoveList generate_all_moves(const Game_State& game) {   
+util::dynamic_array<RankMask, MAX_MOVES_PER_TABLE> generate_all_moves(const Game_State& game) {   
 
-    MoveList available_moves{};
+    util::dynamic_array<RankMask, MAX_MOVES_PER_TABLE> available_moves{};
 
     const RankMask low_table_mask { game.table.cards & LOW_MASK };
 
     for (const Rank& card : current_player_state(game).hand.cards) {
 
-        assert(available_moves.size < MAX_MOVES_PER_TABLE);
-        available_moves.push_back(Move{to_mask(card)});
+        assert(available_moves.size() < MAX_MOVES_PER_TABLE);
+        available_moves.push_back(to_mask(card));
 
         const int rank_idx { rank_to_int(card) };
 
         // todo: make better?
         for (const RankMask addition_pattern : ADDITIONS_BY_RANK[rank_idx]) {
             if ((addition_pattern & low_table_mask) == addition_pattern) {
-                Move move { addition_pattern | to_mask(card) };
+                RankMask move { addition_pattern | to_mask(card) };
                 if (!available_moves.contains(move)) {
-                    assert(available_moves.size < MAX_MOVES_PER_TABLE);
+                    assert(available_moves.size() < MAX_MOVES_PER_TABLE);
                     available_moves.push_back(move);
                 }
             }
--- movegen.h
+++ movegen.h
@@ -1,32 +1,12 @@
 #pragma once
 
 #include "cuarenta.h"
+#include "dynamic_array.h"
 
 #include <vector>
 #include <cassert>
 
 namespace Cuarenta {
-
-struct MoveList {
-    std::array<Move, MAX_MOVES_PER_TABLE> moves{};
-    size_t size = 0;
-
-    static constexpr size_t capacity() { return MAX_MOVES_PER_TABLE; }
-    bool full()  const { return size == capacity(); }
-    bool empty() const { return size == 0; }
-
-    bool contains(const Move& m) const {
-        for (size_t i{}; i < size; i++) {
-            if (m.targets_mask == moves[i].targets_mask) { return true; }
-        }
-        return false;
-    }
-
-    void push_back(const Move& m) {
-        assert(size < capacity());
-        moves[size++] = m;
-    }
-};
 
 static constexpr std::initializer_list<RankMask> ADDITIONS_BY_RANK[] = {
     {},
@@ -56,5 +36,6 @@ constexpr RankMask LOW_MASK { to_mask(Rank::Ace)   | to_mask(Rank::Two)  |
                               to_mask(Rank::Five)  | to_mask(Rank::Six) };
 
 
-MoveList generate_all_moves(const Game_State& game);
+util::dynamic_array<RankMask, MAX_MOVES_PER_TABLE> generate_all_moves(const Game_State& game);
+
 }
