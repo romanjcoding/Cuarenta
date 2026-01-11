f018e6ec567bb8321b79f65eff6313441d8dbe72
Roman Jackson <romanj@uchicago.edu>
2026-01-11 01:21:26 -0600

    minor fixes

--- rank.h
+++ rank.h
@@ -1,22 +1,26 @@
 #pragma once
+#include <iostream>
 #include <cstdint>
 #include <limits>
 #include <bit>
 #include <stdexcept>
 #include <string>
+#include <vector>
+#include <cassert>
 
 namespace Cuarenta {
 
 enum class RankMask : uint16_t {}; // may hold multiple bits as a mask
-enum class Rank     : uint16_t {   // singular-bitted, enforced by helper casts
+enum class Rank     : uint16_t {   // singular-bitted, invariant enforced by helper casts
     Ace=1,
     Two=2, Three=4, Four=8, Five=16, Six=32, Seven=64, 
     Jack=128, Queen=256, King=512, 
     Invalid = 0,
 };
 
-static constexpr int MAX_MOVES_PER_TABLE { 16 };
-static constexpr int NUM_RANK_BITS { std::numeric_limits<std::underlying_type_t<RankMask>>::digits };
+static constexpr size_t MAX_MOVES_PER_TABLE { 16 };
+static constexpr size_t NUM_RANKS { 10 };
+static constexpr size_t NUM_RANK_BITS { std::numeric_limits<std::underlying_type_t<RankMask>>::digits };
 
 // helper casts between Rank, RankMask, and uint16_t
 constexpr uint16_t to_u16(Rank r)      { return static_cast<uint16_t>(r); }
@@ -38,17 +42,17 @@ constexpr bool is_valid_rank(Rank r) {
 
 constexpr Rank to_rank(uint16_t r) {
     Rank rank { static_cast<Rank>(r) };
-    if (is_valid_rank(rank)) { return rank; }
-    else { 
-        throw std::invalid_argument("invalid (non-zero or non-singular bit pattern) casted to Rank.\n"); 
-    }
+    assert(is_valid_rank(rank));
+    return rank;
 }
 constexpr Rank to_rank(RankMask r) {
     Rank rank { static_cast<Rank>(r) };
-    if (is_valid_rank(rank)) { return rank; }
-    else {
-        throw std::invalid_argument("invalid (non-zero or non-singular bit pattern) casted to Rank.\n");
-    }
+    assert(is_valid_rank(rank));
+    return rank;
+}
+
+bool inline contains_ranks(RankMask cards, RankMask ranks) {
+    return (to_u16(cards) & to_u16(ranks)) == to_u16(ranks);
 }
 
 constexpr std::string rank_to_str (Rank rank) {
@@ -97,11 +101,10 @@ constexpr std::string mask_to_str(RankMask mask) {
         return out;
     }
 
-    // " = other1+other2+..."
     out += " = ";
 
     bool first = true;
-    for (int i = 0; i < NUM_RANK_BITS; ++i) {
+    for (size_t i{}; i < NUM_RANK_BITS; i++) {
         uint16_t bit = static_cast<uint16_t>(1u << i);
         if (bits & bit) {
             if (!first) {
@@ -115,13 +118,22 @@ constexpr std::string mask_to_str(RankMask mask) {
     return out;
 }
 
-
 constexpr int rank_to_int (Rank rank) {
-    return NUM_RANK_BITS - std::countl_zero(to_u16(rank));
+    return static_cast<int>(NUM_RANK_BITS) - std::countl_zero(to_u16(rank));
 }
 constexpr Rank int_to_rank(int val) {
-    if (val == 0) return Rank::Invalid;
+    assert(val >= 0 && val < 11);
     return to_rank(static_cast<uint16_t>(1 << (val - 1)));
+}
+
+constexpr std::vector<Rank> mask_to_vector(RankMask mask) {
+    std::vector<Rank> ret;
+    ret.reserve(NUM_RANKS);
+    for (size_t i{1}; i <= NUM_RANKS; i++) {
+        auto rank { int_to_rank(static_cast<int>(i)) };
+        if (contains_ranks(mask, to_mask(rank))) { ret.push_back(rank); }
+    }
+    return ret;
 }
 
 constexpr RankMask operator &(const RankMask& rank_a, const RankMask& rank_b) { 
@@ -133,29 +145,15 @@ constexpr RankMask operator |(const RankMask& rank_a, const RankMask& rank_b) {
 constexpr RankMask operator ~(const RankMask& rank) { 
     return static_cast<RankMask>(~to_u16(rank) & ALL_RANK_BITS);
 }
-constexpr Rank& operator++(Rank& rank, int) {
-    switch(rank) {
-        case Rank::Ace   : return rank = Rank::Two;
-        case Rank::Two   : return rank = Rank::Three;
-        case Rank::Three : return rank = Rank::Four;
-        case Rank::Four  : return rank = Rank::Five;
-        case Rank::Five  : return rank = Rank::Six;
-        case Rank::Six   : return rank = Rank::Seven;
-        case Rank::Seven : return rank = Rank::Jack;
-        case Rank::Jack  : return rank = Rank::Queen;
-        case Rank::Queen : return rank = Rank::King;
-        case Rank::King  : return rank = Rank::Invalid;
-        case Rank::Invalid:
-            throw std::out_of_range("Cannot increment Rank::Invalid");
-    }
+constexpr Rank operator++(Rank& rank, int) {
+    assert(rank != Rank::King);
+    Rank old = rank;
+    rank = to_rank(static_cast<uint16_t>(to_u16(rank) << 1));
+    return old;
 }
 
 constexpr bool operator<(Rank a, Rank b) {
     return rank_to_int(a) < rank_to_int(b);
 }
 
-bool inline contains_ranks(RankMask cards, RankMask ranks) {
-    return (to_u16(cards) & to_u16(ranks)) == to_u16(ranks);
-}
-
-}
+} // namespace Rank
